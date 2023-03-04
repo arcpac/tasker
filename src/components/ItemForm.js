163167876145d3b4b3de9537c14ed9c04e6fba63
antoncab <antonraphaelcaballes@gmail.com>
2023-03-05 00:44:38 +1100

    validation modal

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,56 +1,66 @@
 import React, { useState } from "react";
+import Modal from "./UI/Modal";
 
 function ItemForm(props) {
   const [enteredGoal, setGoal] = useState("");
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState();
 
   const goalChangeHandler = (event) => {
-    if (event.target.value.trim().length > 0) {
-      setIsValid(true);
-    }
     setGoal(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(enteredGoal)
+    console.log(enteredGoal);
 
     if (enteredGoal.trim().length === 0) {
-      setIsValid(false);
+      setIsValid({ title: "Input required", message: "You've entered a blank task" });
       return;
     }
 
     const goalData = {
       title: enteredGoal,
-      status: "active"
+      status: "active",
     };
     props.onSaveGoal(goalData);
     setGoal("");
   };
+  const errorHandler = () => {
+    setIsValid(null);
+  };
 
   return (
-    <form onSubmit={submitHandler} className="m-1 pt-3 pb-3">
-      <div className="row justify-content-end">
-        <div className="col-8">
-          <div className="form-floating mb-3">
-            <input
-              onChange={goalChangeHandler}
-              value={enteredGoal}
-              id="floatingInput"
-              type="text"
-              className={`form-control ${!isValid ? 'is-invalid' : ''}`}
-            ></input>
-            <label htmlFor="floatingInput">Enter task</label>
+    <div>
+      {isValid && (
+        <Modal
+          title={isValid.title}
+          message={isValid.message}
+          onConfirmError={errorHandler}
+        />
+      )}
+      <form onSubmit={submitHandler} className="m-1 pt-3 pb-3">
+        <div className="row justify-content-end">
+          <div className="col-8">
+            <div className="form-floating mb-3">
+              <input
+                onChange={goalChangeHandler}
+                value={enteredGoal}
+                id="floatingInput"
+                type="text"
+                className={`form-control ${isValid ? "is-invalid" : ""}`}
+              ></input>
+              <label htmlFor="floatingInput">Enter task</label>
+            </div>
+          </div>
+
+          <div className="d-grid col-4">
+            <button type="submit" className="btn btn-dark">
+              List
+            </button>
           </div>
         </div>
-
-        <div className="d-grid col-4">
-          <button type="submit" className="btn btn-dark">
-            List
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 export default ItemForm;
